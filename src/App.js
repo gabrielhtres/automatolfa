cc19d5059cdbe9fc361732cf530188ca095c1574
Ana_laura01 <analaura@sitecbr.com.br>
2022-10-26 00:33:07 -0300

    First commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,64 @@
-import logo from './logo.svg';
+import { Box, Button, Modal, Typography } from '@mui/material';
+import { useEffect, useState } from 'react';
+import FormDialog from './Modal';
 import './App.css';
 
 function App() {
+  const [linhas, setLinhas] = useState([]);
+  const [colunas, setColunas] = useState([]);
+  const [marginBotaoColuna, setMarginBotaoColuna] = useState(10);
+  const [marginBotaoLinha, setMarginBotaoLinha] = useState(-100);
+
+  function adicionarColuna() {
+    const novasColunas = [...colunas];
+    novasColunas.push('teste');
+    setColunas(novasColunas);
+  }
+
+  function adicionarLinha() {
+    const novasLinhas = [...linhas];
+    novasLinhas.push('teste');
+    setLinhas(novasLinhas);
+  }
+
+  useEffect(() => {
+    setMarginBotaoColuna(marginBotaoColuna + 50);
+    console.log(marginBotaoColuna);
+  }, [colunas]);
+
+  useEffect(() => {
+    setMarginBotaoLinha(marginBotaoLinha + 20);
+    console.log(marginBotaoLinha);
+  }, [linhas]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <FormDialog />
+      <h1>Automato Finito</h1>
+      <table>
+        <thead>
+          <tr>
+            <th>δ</th>
+            {colunas.map(item => {
+              return(<th>
+                {item}
+              </th>)
+            })}
+          </tr>
+        </thead>
+        <tbody>
+          {linhas.map(item => {
+              return(<tr>
+                <td>
+                  {item}
+                </td>
+              </tr>)
+            })}
+          </tbody>
+      </table>
+      <button className="botao-coluna" onClick={adicionarColuna} style={{ left: marginBotaoColuna }}>+</button>
+      <button className="botao-linha" onClick={adicionarLinha} style={{ top: marginBotaoLinha }}>+</button>
+      </>
   );
 }
 
